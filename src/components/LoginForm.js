0443db4caacee28c521fd2d649d1d3f9c8770cdb
R. Anthony Porter <rah@sheenonline.biz>
2017-05-14 09:20:17 -0400

    Connected LoginForm and emailChanged action

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { emailChanged } from '../actions'
 import { Card, CardSection, Input, Button} from './common'
 
+
 class LoginForm extends Component {
+
+    onEmailChange(text) {
+         this.props.emailChanged(text)
+    }
+
     render() {
         return (
             <Card>
@@ -9,6 +17,7 @@ class LoginForm extends Component {
                     <Input
                         label="Email"
                         placeholder="email@email.com"
+                        onChangeText={this.onEmailChange.bind(this)}
                     />
                 </CardSection>
 
@@ -30,4 +39,4 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm
+export default connect(null,{ emailChanged })(LoginForm)
